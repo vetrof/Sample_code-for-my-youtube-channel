2b89a9c8e83f53a58e68fea2a8a5377cd3716cc5
Vitaly Vetrof <vetrof@gmail.com>
2023-05-15 18:49:11 +0600

    upload

---
+++ func.py
@@ -0,0 +1,38 @@
+
+def myRange(len):
+    digit_list = []
+    counter = 0
+
+    while counter != len:
+        digit_list.append(counter)
+        counter += 1
+
+    return digit_list
+
+
+
+
+
+for i in myRange(123):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
